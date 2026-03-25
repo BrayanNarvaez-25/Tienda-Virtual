0ed3e1cfb923732208f5e84c7bca9f9a7fe717e4
EDWINCSN <c.edwin32@hotmail.com>
2026-03-25 18:59:36 -0500

    feat: add in class Cliente Constructors

--- TiendaVirtual/Cliente.java
+++ TiendaVirtual/Cliente.java
@@ -5,4 +5,18 @@ public class Cliente {
     private double saldo; //Dinero disponible de la cuenta del cliente
     private int totalCompras; //contador de cuantas compras ha realizado el cliente
 
+    
+    //CONSTRUCTOR VACIO
+    public Cliente() {
+        
+    }
+
+    //CONSRUCTOR CON PARAMETROS
+    public Cliente(String nombre, String correo, double saldo) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.saldo = saldo;
+        this.totalCompras = 0;
+    }
+
 }
